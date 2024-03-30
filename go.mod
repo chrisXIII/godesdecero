9551b96a1735f5f955f408a0d620a9ad72658c1c
chrisXIII <cristian_a_rojas@hotmail.com>
2024-03-30 19:21:03 -0300

    Primer Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/chrisXIII/godesdecero
+
+go 1.21.4
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/chrisXIII/godesdecero/variables"
+)
+
+func main() {
+	variables.MuestroEnteros()
+}
