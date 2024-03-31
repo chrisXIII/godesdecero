4166f7b5cf0320e4fa5a8feb89fe6274bcb34812
chrisXIII <cristian_a_rojas@hotmail.com>
2024-03-30 21:44:48 -0300

    Siguiente Commit

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/chrisXIII/godesdecero/variables"
 )
 
 func main() {
 	variables.MuestroEnteros()
+	variables.RestoVariables()
+	estado,texto := variables.ConviertoaTexto(1588)
+	fmt.Println(estado)
+	fmt.Println(texto)
 }
